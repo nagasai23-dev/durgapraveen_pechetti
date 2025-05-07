f9a3f9b5171fe8e41d3fafea53e839427fefbabf
nagasai23-dev <nagasai912@gmail.com>
2025-05-07 22:36:32 +0530

    Minor update

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+// import { useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
