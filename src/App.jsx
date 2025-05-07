e6bae7e9b8bf1d81991dd5b336706dea4cd976bd
nagasai23-dev <nagasai912@gmail.com>
2025-05-07 22:23:17 +0530

    Fixed import path for Home component

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
-import Home from './pages/Home/Home';
+import Home from './pages/Home/Home.jsx';
 import About from './pages/About/About';
 import Work from './pages/Work/Work';
 import ContactPage from './pages/Contact/ContactPage';
