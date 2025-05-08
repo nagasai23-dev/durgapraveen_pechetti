4c42b96a9000a196ce29579e9186b664d0629151
nagasai23-dev <nagasai912@gmail.com>
2025-05-08 10:06:26 +0530

    Change vite config

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: process.env.VITE_BASE_PATH || "/durgapraveen_pechetti",
   resolve: {
     extensions: ['.js', '.jsx', '.json'] // Add this resolve block
   },
